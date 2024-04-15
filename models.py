e23aec124d0d48d949cc33c9aa932cff682d340f
dlct-wzx <18109232165@163.com>
2024-04-15 16:10:48 +0800

    4.16

--- models.py
+++ models.py
@@ -1,3 +1,6 @@
+import tifffile as tf
+import numpy as np
+
 class Model:
     def __init__(self):
         self.modelList = ['DBFormer', 'SegFormer', 'MaskFormer', 'SETR', 'DeepLabV3+', 'PSPNet', 'UNet', 'HRNetV2']
@@ -11,67 +14,76 @@ class Model:
             'UNet': ['ResNet50'],
             'HRNetV2': ['HRNetV2-w18', 'HRNetV2-w32', 'HRNetV2-w48']
         }
+        self.models = dict()
 
     def generateModel(self, modelName, encoderName):
-        model = '111'
-
+        model = None
         if modelName == 'DBFormer':
-            if self.__dict__['DBFormer_' + encoderName] is None:
+            if 'DBFormer_' + encoderName not in self.models:
                 model = '222'
-                self.__dict__['DBFormer_' + encoderName] = model
+                self.models['DBFormer_' + encoderName] = model
             else:
-                model = self.__dict__['DBFormer_' + encoderName]
+                model = self.models['DBFormer_' + encoderName]
         elif modelName == 'SegFormer':
-            if self.__dict__['SegFormer_' + encoderName] is None:
+            if 'SegFormer_' + encoderName not in self.models:
                 model = '333'
-                self.__dict__['SegFormer_' + encoderName] = model
+                self.models['SegFormer_' + encoderName] = model
             else:
-                model = self.__dict__['SegFormer_' + encoderName]
+                model = self.models['SegFormer_' + encoderName]
         elif modelName == 'MaskFormer':
-            if self.__dict__['MaskFormer_' + encoderName] is None:
+            if 'MaskFormer_' + encoderName not in self.models:
                 model = '444'
-                self.__dict__['MaskFormer_' + encoderName] = model
+                self.models['MaskFormer_' + encoderName] = model
             else:
-                model = self.__dict__['MaskFormer_' + encoderName]
+                model = self.models['MaskFormer_' + encoderName]
         elif modelName == 'SETR':
-            if self.__dict__['SETR_' + encoderName] is None:
+            if 'SETR_' + encoderName not in self.models:
                 model = '555'
-                self.__dict__['SETR_' + encoderName] = model
+                self.models['SETR_' + encoderName] = model
             else:
-                model = self.__dict__['SETR_' + encoderName]
+                model = self.models['SETR_' + encoderName]
         elif modelName == 'DeepLabV3+':
-            if self.__dict__['DeepLabV3+_' + encoderName] is None:
+            if 'DeepLabV3+_' + encoderName not in self.models:
                 model = '666'
-                self.__dict__['DeepLabV3+_' + encoderName] = model
+                self.models['DeepLabV3+_' + encoderName] = model
             else:
-                model = self.__dict__['DeepLabV3+_' + encoderName]
+                model = self.models['DeepLabV3+_' + encoderName]
         elif modelName == 'PSPNet':
-            if self.__dict__['PSPNet_' + encoderName] is None:
+            if 'PSPNet_' + encoderName not in self.models:
                 model = '777'
-                self.__dict__['PSPNet_' + encoderName] = model
+                self.models['PSPNet_' + encoderName] = model
             else:
-                model = self.__dict__['PSPNet_' + encoderName]
+                model = self.models['PSPNet_' + encoderName]
         elif modelName == 'UNet':
-            if self.__dict__['UNet_' + encoderName] is None:
+            if 'UNet_' + encoderName not in self.models:
                 model = '888'
-                self.__dict__['UNet_' + encoderName] = model
+                self.models['UNet_' + encoderName] = model
             else:
-                model = self.__dict__['UNet_' + encoderName]
+                model = self.models['UNet_' + encoderName]
         elif modelName == 'HRNetV2':
-            if self.__dict__['HRNetV2_' + encoderName] is None:
+            if 'HRNetV2_' + encoderName not in self.models:
                 model = '999'
-                self.__dict__['HRNetV2_' + encoderName] = model
+                self.models['HRNetV2_' + encoderName] = model
             else:
-                model = self.__dict__['HRNetV2_' + encoderName]
+                model = self.models['HRNetV2_' + encoderName]
 
         return model
 
     def detect(self, modelName, encoderName, redPath, greenPath, bluePath, nirPath, rePath):
         model = self.generateModel(modelName, encoderName)
-        print(modelName)
-        print(encoderName)
+        print('use model ' + model + ' to detect')
         print(redPath)
         print(greenPath)
         print(bluePath)
         print(nirPath)
         print(rePath)
+        red = tf.imread(redPath)[np.newaxis, :]
+        green = tf.imread(greenPath)[np.newaxis, :]
+        blue = tf.imread(bluePath)[np.newaxis, :]
+        nir = tf.imread(nirPath)[np.newaxis, :]
+        re = tf.imread(rePath)[np.newaxis, :]
+        rvi = red / nir
+        rvi = np.where(rvi < 0.8, 0, 1)
+        tif = np.concatenate(([blue, green, red, re, nir, rvi]), axis=0)
+        print(tif.shape)
+
--- show.py
+++ show.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import tifffile as tf
+import numpy as np
 from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtGui import QPixmap
 from shutil import copyfile
@@ -7,6 +9,43 @@ from ToolsUI import Ui_Dialog
 from qt_material import apply_stylesheet
 from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QLineEdit, QMainWindow, QFileDialog
 from models import Model
+
+
+def incorporatedPhaseAlignment(imageName, bindType):
+    image = tf.imread(imageName)
+    os.remove(imageName)
+    W, L = image.shape
+    if bindType == 'blue':
+        pos_x = -17
+        pos_y = -19
+    elif bindType == 'green':
+        pos_x = -25
+        pos_y = -14
+    elif bindType == 'nir':
+        pos_x = -27
+        pos_y = -4
+    elif bindType == 'red':
+        pos_x = -14
+        pos_y = -10
+    elif bindType == 'red_edge':
+        pos_x = 0
+        pos_y = 21
+
+    # print(pos_x, pos_y)
+    pos_y1 = abs(pos_y)
+    pos_x1 = abs(pos_x)
+
+    image_pad = np.pad(image, ((pos_y1, pos_y1), (pos_x1, pos_x1)), 'constant', constant_values=(0, 0))
+    if pos_y < 0:  # y轴负方向，图像上移
+        image = image_pad[0:L, :]
+    else:  # y轴正方向，图像下移
+        image = image_pad[pos_y1 * 2:L + pos_y1 * 2, :]
+    if pos_x < 0:  # x轴负方向，图像左移
+        image = image[:, pos_x1 * 2:W + pos_x1 * 2]
+    else:  # x轴正方向，图像右移
+        image = image[:, 0:W]
+    tf.imwrite(imageName, image)
+
 
 class MyPyQT_Form(QtWidgets.QWidget, Ui_Dialog):
     def __init__(self):
@@ -26,16 +65,29 @@ class MyPyQT_Form(QtWidgets.QWidget, Ui_Dialog):
         self.ReSave.clicked.connect(self.pushSaveButton_click)
         self.ResultSave.clicked.connect(self.pushSaveButton_click)
 
+        self.IncorporatedPhaseAlignment.clicked.connect(self.pushIncorporatedPhaseAlignmentButton_click)
+
+        self.VI.clicked.connect(self.pushVIButton_click)
+
         self.ModelSet.currentIndexChanged.connect(self.modelSet)
         self.Seg.clicked.connect(self.modelDetect)
+
         self.CachaPath = "./Cache/"
         self.GreenPath = self.CachaPath + "Green.TIF"
         self.RedPath = self.CachaPath + "Red.TIF"
         self.BluePath = self.CachaPath + "Blue.TIF"
         self.NIRPath = self.CachaPath + "NIR.TIF"
         self.RePath = self.CachaPath + "RE.TIF"
+        self.VIPath = self.CachaPath + "VI.TIF"
 
         self.model = Model()
+
+    def flashAllImage(self):
+        self.RedBind.setPixmap(QPixmap(self.RedPath).scaled(320, 260))
+        self.GreenBind.setPixmap(QPixmap(self.GreenPath).scaled(320, 260))
+        self.BlueBind.setPixmap(QPixmap(self.BluePath).scaled(320, 260))
+        self.NIRBind.setPixmap(QPixmap(self.NIRPath).scaled(320, 260))
+        self.ReBind.setPixmap(QPixmap(self.RePath).scaled(320, 260))
 
     def pushSetButton_click(self):
         buttonName = self.sender().objectName()
@@ -56,6 +108,38 @@ class MyPyQT_Form(QtWidgets.QWidget, Ui_Dialog):
                                                       "all files(*)")
         copyfile(ImagePath, saveFilePath)
 
+    def pushIncorporatedPhaseAlignmentButton_click(self):
+        print('IncorporatedPhaseAlignment is clicked')
+        incorporatedPhaseAlignment(self.RedPath, 'red')
+        print('red is done')
+        incorporatedPhaseAlignment(self.GreenPath, 'green')
+        incorporatedPhaseAlignment(self.BluePath, 'blue')
+        incorporatedPhaseAlignment(self.NIRPath, 'nir')
+        incorporatedPhaseAlignment(self.RePath, 'red_edge')
+        print('green is done')
+        self.flashAllImage()
+
+    def pushVIButton_click(self):
+        print('VI is clicked')
+        VIName = self.VISet.currentText()
+        red = tf.imread(self.RedPath)
+        green = tf.imread(self.GreenPath)
+        blue = tf.imread(self.BluePath)
+        nir = tf.imread(self.NIRPath)
+        re = tf.imread(self.RePath)
+        if VIName == 'NDVI':
+            print('NDVI is selected')
+            vi = (nir - red) / (nir + red)
+        elif VIName == 'EVI':
+            print('EVI is selected')
+            vi = 2.5 * (nir - red) / (nir + 6 * red - 7.5 * blue + 1)
+        elif VIName == "RVI":
+            print('RVI is selected')
+            vi = red / nir
+        print('VI is done')
+        tf.imwrite(self.VIPath, vi)
+        self.Result.setPixmap(QPixmap(self.VIPath).scaled(320, 260))
+
     def modelSet(self):
         modelName = self.ModelSet.currentText()
         print(modelName + ' is selected')
@@ -66,10 +150,9 @@ class MyPyQT_Form(QtWidgets.QWidget, Ui_Dialog):
     def modelDetect(self):
         modelName = self.ModelSet.currentText()
         encoderName = self.EncoderSet.currentText()
-        print(modelName + ' is selected')
-        print(encoderName + ' is selected')
-        self.model.detect(modelName, encoderName, self.RedPath, self.GreenPath, self.BluePath, self.NIRPath, self.RePath)
-
+        print('use model ' + modelName + 'and encoder ' + encoderName + ' to detected')
+        self.model.detect(modelName, encoderName, self.RedPath, self.GreenPath, self.BluePath, self.NIRPath,
+                          self.RePath)
 
     def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
         print('close')
